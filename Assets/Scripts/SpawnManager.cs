efe4a95b1761dd1167b8cf31d06e42fc412d691e
Jorge Medina <jorgemedinaleigh@protonmail.com>
2021-09-24 19:06:08 -0300

    Spawn obstacles at random positions and times

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,26 +4,43 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    [SerializeField] GameObject obstacle;
-    [SerializeField] float startDelay = 0f;
-    [SerializeField] float repeatRate = 3f;
+    [SerializeField] GameObject[] obstacles;
+    [SerializeField] float minRepeatRate = 1f;
+    [SerializeField] float maxRepeatRate = 5f;
 
-    private Vector3 spawnPos = new Vector3(0, 0, 45);
+    private Vector3 spawnPos;
+    private float waitTime;
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        waitTime -= Time.deltaTime;
+        if(waitTime <= 0)
+        {
+            SpawnObstacle();
+            SetTimer();
+        }
     }
 
     private void SpawnObstacle()
     {
-        Instantiate(obstacle, spawnPos, obstacle.transform.rotation);
+        float range = 4.5f;
+        float xPos = Random.Range(-range, range);
+        spawnPos = new Vector3(xPos, 0, 30);
+
+        int randomIndex = Random.Range(0, obstacles.Length);
+
+        Instantiate(obstacles[randomIndex], spawnPos, obstacles[randomIndex].transform.rotation);
+    }
+
+    private void SetTimer()
+    {
+        waitTime = Random.Range(minRepeatRate, maxRepeatRate);
     }
 }
